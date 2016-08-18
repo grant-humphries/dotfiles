f0bade49868b648215cd1953d6291910c7d7fbe1
grant-humphries <grant.humphries@gmail.com>
2016-08-17 17:51:25 -0700

    added clause to avoid linking .minttyrc on none windows machines

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -14,13 +14,17 @@ dot_files=(
 
 old_dotfiles="${HOME}/old_dotfiles"
 mkdir -p "${old_dotfiles}"
-dotfiles_repo="$( cd  $(dirname ${0}); dirname $(pwd -P) )"
+dotfiles_repo=$( cd  $(dirname ${0}); dirname $(pwd -P) )
 
 for df in "${dot_files[@]}"; do
+    # .minttyrc is only used on windows
+    if [[ "${OSTYPE}" != 'cygwin' && "${df}" == '.minttyrc' ]]; then
+        continue
+    fi
+
     existing_df="${HOME}/${df}"
     if [ -f "${existing_df}" ]; then
         mv "${existing_df}" "${old_dotfiles}/"
-    
     fi
     
     ln -sf "${dotfiles_repo}/${df}" "${HOME}/${df}"
