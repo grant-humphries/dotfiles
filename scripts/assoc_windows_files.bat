d10dfa9ba9deeda2e18f17a51ab2c2b19e38e04e
grant-humphries <grant.humphries@gmail.com>
2016-09-07 15:58:01 -0700

    added script for file associations on windows

---
+++ scripts/assoc_windows_files.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:: on my new windows 10 install these files associations are broken,
+:: these commands will repair them (this script must be run
+::adminstrator from cmd)
+assoc .bat=batfile
+assoc .cmd=cmdfile
+assoc .vbs=vbsfile
