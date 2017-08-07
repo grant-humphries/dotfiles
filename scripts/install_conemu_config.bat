7cd283c97e8b6db93e053f908241672970aaa193
grant-humphries <grant.humphries@gmail.com>
2017-08-07 11:29:01 -0700

    Add install script for ConEmu config

---
+++ scripts/install_conemu_config.bat
@@ -0,0 +1,12 @@
+::@echo off
+setlocal enableDelayedExpansion
+
+:: get repo directory
+for %%i in ("%~dp0..") do set "dotfiles_path=%%~fi"
+
+:: symlink the ComEmu config file from the dotfiles repo and move the
+:: original config file so it won't be read
+cd "%ConEmuDir%"
+mklink ".\.ConEmu.xml" "%dotfiles_path%\.ConEmu.xml"
+move ".\ConEmu.xml" ".\ConEmu.xml.old"
+q
