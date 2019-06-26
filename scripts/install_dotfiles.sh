7b2d1fd7624b5d21b333cc3636d9f375709de47e
grant-humphries <grant.humphries@gmail.com>
2019-06-25 18:25:51 -0700

    Refactor and augment git config setup
    * Set all bash and cmd scripts executable in the git index
    * Refactor post_checkout hook to use functions
    * Add function to post_checkout hook to clear file permission tracking
    at the repo level when using cygwin
    * Create git alias that gives files the execute bit in the git index
    (for use when file mode tracking is disabled)
    * Make all setting camelCase in gitconfig
    * Adjust git config settings so all repos consume git hooks from a
    central source rather than having them copied into each repo on
    clone/init
    * Create Windows specific gitconfig and in the dotfiles install script
    install it only on Windows
    * Refactor dotfiles install script to use functions and local variables,
    remove support for msys, and improve handling of installation of windows
    specific files

--- scripts/install_dotfiles.sh
+++ scripts/install_dotfiles.sh
@@ -1,50 +1,50 @@
 #!/usr/bin/env bash
 set -e
 
-dotfiles=(
-    '.bashrc'
-    '.bash_logout'
-    '.bash_profile'
-    '.inputrc'
-    '.gitconfig'
-    '.git-completion.bash'
-    '.git-prompt.sh'
-    '.git-templates'
-    '.minttyrc'
-    '.vim'
-    '.vimrc'
-)
+install_dotfiles() {
+    local dotfiles=(
+        '.bashrc'
+        '.bash_logout'
+        '.bash_profile'
+        '.inputrc'
+        '.gitconfig'
+        '.git-completion.bash'
+        '.git-prompt.sh'
+        '.git-templates'
+        '.vim'
+        '.vimrc'
+    )
 
-win_unix=( 'cygwin' 'msys' )
-default_dotfiles='/tmp/default_dotfiles'
-dotfiles_repo=$( cd $(dirname ${0}); dirname $(pwd -P) )
-
-mkdir -p "${default_dotfiles}"
-
-for df in "${dotfiles[@]}"; do
-    # .minttyrc is only used on windows
-    if [[ ! "${win_unix[@]}" =~ "${OSTYPE}" && "${df}" == '.minttyrc' ]]; then
-        continue
+    # add Windows specific configs if on that platfrom
+    if [[ "${OSTYPE}" == 'cygwin' ]]; then
+        dotfiles+=(
+            '.gitconfig.windows'
+            '.minttyrc'
+        )
     fi
 
-    target="${dotfiles_repo}/${df}"
-    link="${HOME}/${df}"
+    local dotfiles_repo=$( cd $(dirname ${0}); dirname $(pwd -P) )
+    local default_dotfiles='/tmp/default_dotfiles'
 
-    # check if file or symlink already exists in link location
-    if [ -e "${link}" ]; then
-        mv "${link}" "${default_dotfiles}/"
-        mv_flag=1
+    mkdir -p "${default_dotfiles}"
+
+    for df in "${dotfiles[@]}"; do
+        local source="${dotfiles_repo}/${df}"
+        local link="${HOME}/${df}"
+
+        # check if file or symlink already exists in link location
+        if [[ -e "${link}" ]]; then
+            mv "${link}" "${default_dotfiles}/"
+            mv_flag=1
+        fi
+
+        ln -s "${source}" "${link}"
+    done
+
+    if [[ -n "${mv_flag}" ]]; then
+        echo 'some dotfiles already existed in your home directory, they have '
+        echo "been moved to the following directory: ${default_dotfiles}"
     fi
+}
 
-    # if in running msys a windows symlink must be created
-    if [[ "${OSTYPE}" == 'msys' ]]; then
-        cmd //c mklink "$(cygpath -w ${link})" "$(cygpath -w ${target})"
-    else
-        ln -s "${target}" "${link}"
-    fi
-done
-
-if [ -n "${mv_flag}" ]; then
-    echo 'some dotfiles already existed in your home directory, they have '
-    echo "been moved to the following directory: ${default_dotfiles}"
-fi
+install_dotfiles
