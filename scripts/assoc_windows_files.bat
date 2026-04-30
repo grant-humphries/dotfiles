710a82da02d74f0b1371d44d2b5b95e90303f358
grant-humphries <grant.humphries@gmail.com>
2026-04-30 10:09:19 -0700

    Get rid of old batch script

--- scripts/assoc_windows_files.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-:: on my new windows 10 install these files associations are broken,
-:: these commands will repair them (this script must be run
-::adminstrator from cmd)
-assoc .bat=batfile
-assoc .cmd=cmdfile
-assoc .vbs=vbsfile
